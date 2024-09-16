66a9392d506e975a74f42321b500fd3af3ee4c68
l.maines <lmmaines28@gmail.com>
2024-09-15 20:32:08 -0400

    reconfiguring to get server working

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
-app.use(express.static('../client/dist'));
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.urlencoded({ extended: true }));
 
-require('./routes/htmlRoutes')(app);
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
+
+// require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
